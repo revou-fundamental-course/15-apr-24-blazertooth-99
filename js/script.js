b4af2c93e99e272f0703f27874395d19ff03083c
Lancea <christiansatrio30@gmail.com>
2024-04-23 20:45:10 +0700

    Second Commit

--- js/script.js
+++ js/script.js
@@ -1,61 +1,83 @@
-// constructor IO Fahrenheit
-const inputFahrenheit = document.getElementById("inputFahrenheit")
+const inputFahrenheit = document.getElementById("inputFahrenheit") // constructor IO Fahrenheit
 const outputFahrenheit = document.getElementById("outputFahrenheit")
-// constructor IO Celcius
-const inputCelcius = document.getElementById("inputCelcius")
+const inputCelcius = document.getElementById("inputCelcius") // constructor IO Celcius
 const outputCelcius = document.getElementById("outputCelcius")
-// constructor Button
-const buttonConvert = document.getElementById("result") // Convert Button
+const buttonConvertF = document.getElementById("resultF") // constructor Button convert Fahrenheit
+const buttonConvert = document.getElementById("result") // constructor Button convert Celcius
 const buttonReset = document.getElementById("reset") // Reset button div Celcius
 const buttonResetF = document.getElementById("resetF") // Reset button div Fahrenheit
-const switchMode = document.getElementById("reverse")
-// constructor print Formula
-const getFormulaCelcius = document.getElementById("formulaC")
-const getFormulaFahrenheit = document.getElementById("formulaF")
+const switchMode = document.getElementById("reverse") // Reverse button C to F / F to C
+const getFormulaCelcius = document.getElementById("formulaC") // constructor print Formula
+const getFormulaFahrenheit = document.getElementById("formulaF") // constructor print Formula
 
-const error = document.getElementById("error")
+// const error = document.getElementById("error")
 // const modeText1 = document.getElementById("modeText1")
 // const modeText2 = document.getElementById("modeText2")
 
-// <----------------- SCRIPT ----------------->
 
-// Regex Allow input number & dot only Celcius
-const valInputC = document.getElementById("inputCelcius");
-valInputC.onchange = valInputC.oninput = () => {   
-valInputC.value = valInputC.value.replace(/[^\d\.]/g, "") .replace(/\./, "x") .replace(/\./g, "") .replace(/x/, "."); 
+// <--------------------- VALIDATION --------------------->
+
+// Regex Allow input : number;dot;minus(-) Celcius
+inputCelcius.onchange = inputCelcius.oninput = () => {   
+inputCelcius.value = inputCelcius.value.replace(/[^\d\.\-]/g, "") .replace(/\./, "x") .replace(/\./g, "") .replace(/x/, "."); 
 }
 
-// Regex Allow input number & dot only Fahrenheit
-const valInputF = document.getElementById("inputFahrenheit");
-valInputF.onchange = valInputF.oninput = () => {   
-valInputF.value = valInputF.value.replace(/[^\d\.]/g, "") .replace(/\./, "x") .replace(/\./g, "") .replace(/x/, "."); 
+// Regex Allow input : number;dot;minus(-) Fahrenheit
+inputFahrenheit.onchange = inputFahrenheit.oninput = () => {   
+inputFahrenheit.value = inputFahrenheit.value.replace(/[^\d\.-]/g, "") .replace(/\./, "x") .replace(/\./g, "") .replace(/x/, "."); 
 }
+
+// Disable button convert when input is Null Celcius
+function disabledInput()
+  {
+    if(inputCelcius.value === '') {
+      buttonConvert.disabled = true;
+      }else {
+      buttonConvert.disabled = false;
+    }
+ }
+
+// Disable button convert when input is Null Fahrenheit
+ function disabledInputF() 
+ {
+  if(inputFahrenheit.value === '') {
+    buttonConvertF.disabled = true;
+    }else {
+    buttonConvertF.disabled = false;
+  }
+ }
+// <--------------------- END VALIDATION --------------------->
+
+// <--------------------- FUNCTION BUTTON --------------------->
 
 // Button Reset div Celcius
 buttonReset.addEventListener("click", () => {
-
-    // Melakukan reset ke value awal atau semula
-    error.innerHTML = ''
-    inputCelcius.value = ''
-    outputFahrenheit.value = ''
-    getFormulaCelcius.innerHTML = ''
-})
+  inputCelcius.value = '';
+  outputFahrenheit.value = '';
+  getFormulaCelcius.innerHTML = '';
+  if (inputCelcius.value === ''){
+    buttonConvert.disabled = true;
+  }else {
+    buttonConvert.disabled = false;
+  }
+},)
 
 // Button Reset div Fahrenheit
 buttonResetF.addEventListener("click", () => {
-
-  // Melakukan reset ke value awal atau semula
-  error.innerHTML = ''
-  outputCelcius.value = ''
-  inputFahrenheit.value = ''
-  getFormulaFahrenheit.innerHTML = ''
+  inputFahrenheit.value = '';
+  outputCelcius.value = '';
+  getFormulaFahrenheit.innerHTML = '';
+  if (inputFahrenheit.value === ''){
+    buttonConvertF.disabled = true;
+  }else {
+    buttonConvertF.disabled = false;
+  }
 })
 
-
+// Button Swap div Celcius - Fahrenheit to div Fahrenheit - Celcius
 function swap() {
   let divCF = document.getElementById("divCF");
   let divFC = document.getElementById("divFC");
-
   if ( divFC.style.display == "none") {
     divCF.style.display = "none";
     divFC.style.display = "block";
@@ -68,35 +90,29 @@ function swap() {
  function cf()
  {
    const inputCelcius = document.getElementById("inputCelcius").value;
-   let formulaCF = (inputCelcius * (9 / 5 )) + 32;
-   let resultCF = document.getElementById("outputFahrenheit").value = formulaCF.toFixed(2);
-
-    //  Print formula Celcius to Fahrenheit (0 °C × 9/5) + 32 = 32 °F
-  getFormulaCelcius.innerHTML = '(' + inputCelcius + ' °C × 9/5) + 32 = ' + resultCF+' °F';  
+// Validate input if null
+   if (inputCelcius === ''){
+    alert("Inputan Celcius (° C) tidak boleh 'NULL' :)")
+    }else {
+      let formulaCF = (inputCelcius * (9 / 5 )) + 32;
+      let resultCF = document.getElementById("outputFahrenheit").value = formulaCF.toFixed(2);
+// Print formula Celcius to Fahrenheit (0 °C × 9/5) + 32 = 32 °F
+    getFormulaCelcius.innerHTML = '(' + inputCelcius + ' °C × 9/5) + 32 = ' + resultCF+' °F'; 
+  }
  }
 
  function fc()
  {
    const inputFahrenheit = document.getElementById("inputFahrenheit").value;
-   let formulaFC = (inputFahrenheit - 32 ) * (5 / 9);
-   let resultFC = document.getElementById("outputCelcius").value = formulaFC.toFixed(2);
+// Validate input if null
+   if (inputFahrenheit === ''){
+    alert("Inputan Fahrenheit (° F) tidak boleh 'NULL' :)")
+    }else {
+      let formulaFC = (inputFahrenheit - 32 ) * (5 / 9);
+      let resultFC = document.getElementById("outputCelcius").value = formulaFC.toFixed(2);
+// Print formula Fahrenheit to Celcius (0 °C × 9/5) + 32 = 32 °F
+    getFormulaFahrenheit.innerHTML = '(' + inputFahrenheit + ' °F -32) × (5/9)  = ' + resultFC +' °C';
+  }
+}
 
-  //  Print formula Fahrenheit to Celcius (0 °C × 9/5) + 32 = 32 °F
-  getFormulaFahrenheit.innerHTML = '(' + inputFahrenheit + ' °F -32) × (5/9)  = ' + resultFC +' °C';
- }
-
-
-
-
-
-
-
-
-
-// if (tempType.value === "fahrenheit") {
-//     const CelciusToFahrenheit = (inputValue * 1.8) + 32;
-//     celsiusElem.innerHTML = `${CelciusToFahrenheit} &deg;F`;
-//   } else if (tempType.value === "celcius") {
-//     const FahrenheitToCelsius  = (inputValue - 32) * (1.8);
-//     celsiusElem.innerHTML = `${FahrenheitToCelsius.toFixed(3)} &deg;c`;
-//   }
+// <--------------------- END FUNCTION BUTTON --------------------->
